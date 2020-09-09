a524890535f3ff12d8e89792d15bb16cd32416a0
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2020-09-09 16:38:41 +0100

    some tests

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
-const app = require('./app');
-
+const app = require('./app')
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://malik:google44644@cluster0.3uo0s.mongodb.net/test?retryWrites=true&w=majority";
 
@@ -8,8 +7,7 @@ MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, client){
         console.log(err)
     }else{
         console.log("the database is connected")
-        module.exports = client.db("test")
-        const app = require('./app')
+        module.exports = client.db("flutter_app")
         app.listen(5000, () => {
             console.log("server is listening at port 5000")
         })
--- routes/route.js
+++ routes/route.js
@@ -2,9 +2,17 @@ const express = require("express")
 
 const router = express.Router()
 
+// global controller
+router.get('/*',function(req,res,next){
+    res.set('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 router.get("/", (req, res) => {
-    res.send("this works")
+    let db = require("../db")
+    db.collection("users").find().toArray().then((i) => {
+        res.send(i)
+    })
     console.log("\nGET is requested")
 })
 
