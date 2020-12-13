b023ef95f3778a8e0af3e76214f3b41e8352cf41
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2020-12-13 17:16:01 +0100

    fixing some things

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,23 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 // controllers
-let productsController = require("../controller/productsController")
-let categoriesController = require("../controller/categoriesController")
+let productsController = require("../controller/productsController");
+let categoriesController = require("../controller/categoriesController");
 
 router.get("/", (req, res) => {
     res.send("this is the api page");
 })
 
 router.get('/products', function (req, res) {
-    if(req.query.category == null){
-        productsController.getProducts().then((result) => {
-            res.json(result);
-        });
-    }else if(req.query.category != null){
-        productsController.getProductsByCategory(req.query.category).then((result) => {
-            res.json(result);
-        })
-    }
+    productsController.getProducts().then((result) => {
+        res.json(result);
+    });
+})
+
+router.get('/products/:category', function (req, res) {
+    productsController.getProductsByCategory(req.params.category).then((result) => {
+        res.json(result);
+    })
 })
 
 router.get('/categories', function (req, res) {
