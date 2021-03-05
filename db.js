75545875cd72690415f78563dd9bcacd8401afcf
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-03-05 03:29:36 +0100

    commenting the dot

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
-//ading envirement variables
-require('dotenv').config();
+// //ading envirement variables
+// require('dotenv').config();
 
 const pool = mysql.createPool({
 	connectionLimit: 10,
