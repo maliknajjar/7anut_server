2d5b16b51d5bbce327893080b60ed493b03df3ec
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-04-04 16:48:37 +0100

    progress

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -2,7 +2,7 @@ let productsModels = require("../models/productsModels");
 
 let controllers = {
     getProducts: function(req, res){
-        productsModels.getProducts(req.params)
+        productsModels.getProducts(req.params.store)
         .then((products) => {
             res.json(products);
         })
