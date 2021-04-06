126127569b3db9d9065094519794df7aa06f96d7
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-04-06 01:27:11 +0100

    security addition (update)

--- models/addressesModels.js
+++ models/addressesModels.js
@@ -27,9 +27,20 @@ let models = {
     },
     swapuseraddresses: function(object){
         return new Promise(function(resolve, reject){
-            db.query(`UPDATE addresses t1 INNER JOIN addresses t2 ON (t1.ID, t2.ID) IN ((${db.escape(object.top)},${db.escape(object.bottom)}),(${db.escape(object.bottom)},${db.escape(object.top)})) SET t1.addresse = t2.addresse WHERE userEmail = ${db.escape(object.email)}`, function (error, result) {
+            db.query(`SELECT * FROM addresses WHERE ID = ${db.escape(object.top)} OR ID = ${db.escape(object.bottom)}`, function (error, result) {
                 if (error) throw error;
-                resolve(result)
+                if(result[0].userEmail != object.email){
+                    resolve("the address is not yours")
+                    return;
+                }
+                if(result[1].userEmail != object.email){
+                    resolve("the address is not yours")
+                    return;
+                }
+                db.query(`UPDATE addresses t1 INNER JOIN addresses t2 ON (t1.ID, t2.ID) IN ((${db.escape(object.top)},${db.escape(object.bottom)}),(${db.escape(object.bottom)},${db.escape(object.top)})) SET t1.addresse = t2.addresse`, function (error, result) {
+                    if (error) throw error;
+                    resolve(result)
+                });
             });
         })
     },
