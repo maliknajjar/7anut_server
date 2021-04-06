76b7d2dcd026e2b9d89d0632beed6f3491a73c4f
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-04-06 02:00:18 +0100

    making the reserving items systeme

--- models/productsModels.js
+++ models/productsModels.js
@@ -11,7 +11,7 @@ let models = {
     },
     takeproduct: function(object){
         return new Promise(function(resolve, reject){
-            db.query(`UPDATE products SET amount = amount + 1 WHERE ID = ${db.escape(object.ID)}`, function (error, result) {
+            db.query(`UPDATE products SET amount = amount - 1 WHERE ID = ${db.escape(object.ID)}`, function (error, result) {
                 if (error) throw error;
                 resolve(result)
             });
@@ -19,7 +19,7 @@ let models = {
     },
     leaveproduct: function(object){
         return new Promise(function(resolve, reject){
-            db.query(`UPDATE products SET amount = amount - 1 WHERE ID = ${db.escape(object.ID)}`, function (error, result){
+            db.query(`UPDATE products SET amount = amount + 1 WHERE ID = ${db.escape(object.ID)}`, function (error, result){
                 if (error) throw error;
                 resolve(result)
             });
