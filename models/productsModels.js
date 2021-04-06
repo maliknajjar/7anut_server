d590e5bbbebf1acb42852a55ae726e9770a476f7
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-04-06 16:07:59 +0100

    add out of stock notification feature

--- models/productsModels.js
+++ models/productsModels.js
@@ -11,12 +11,19 @@ let models = {
     },
     takeproduct: function(object){
         return new Promise(function(resolve, reject){
-            db.query(`UPDATE products SET amount = amount - 1 WHERE ID = ${db.escape(object.ID)}`, function (error, result) {
+            db.query(`SELECT * FROM products WHERE ID = ${db.escape(object.ID)}`, function (error, r) {
                 if (error) throw error;
-                // edit user basket also
-                db.query(`UPDATE users SET basket = ${db.escape(object.basket)} WHERE email = ${db.escape(object.email)}`, function (error, theresult) {
+                if (r[0].amount == 0){
+                    resolve("product finished")
+                    return;
+                }
+                db.query(`UPDATE products SET amount = amount - 1 WHERE ID = ${db.escape(object.ID)}`, function (error, result) {
                     if (error) throw error;
-                    resolve(theresult)
+                    // edit user basket also
+                    db.query(`UPDATE users SET basket = ${db.escape(object.basket)} WHERE email = ${db.escape(object.email)}`, function (error, theresult) {
+                        if (error) throw error;
+                        resolve(theresult)
+                    });
                 });
             });
         })
