97ed6637ebe513c6ccceedbdef63effbb38ff509
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-04-25 09:31:25 +0100

    cleaning test for docker

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
 	connectionLimit: 10,
-	host: "sasas",
+	host: process.env.HOST,
 	user: process.env.USER,
 	password: process.env.PASSWORD,
 	database: process.env.DATABASE,
