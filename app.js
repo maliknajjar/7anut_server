d95e73601f265720d5cd9b321617fe9a13de32a3
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-04-25 07:51:22 +0100

    cleaning test for docker

--- app.js
+++ app.js
@@ -103,6 +103,29 @@ setInterval(function ping() {
     });
 }, 60000 * 60);
 
+/////////////////////////////////////
+//          exit clean up          //
+/////////////////////////////////////
+
+//do something when app is closing
+process.on('exit', () => {
+    console.log("cleaned the server");
+    process.exit();
+});
+
+// //catches ctrl+c event
+// process.on('SIGINT', () => {
+//     console.log("cleaned the server");
+//     process.exit();
+// });
+
+// // catches "kill pid" (for example: nodemon restart)
+// process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+// process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+// //catches uncaught exceptions
+// process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
 module.exports = {
     "server": server,
     "wss": wss
