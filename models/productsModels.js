d7d7d259e9ef9af64ccfb0f8d1718aa3e32e517f
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-04-28 06:02:11 +0100

    making the server check for limit

--- models/productsModels.js
+++ models/productsModels.js
@@ -15,12 +15,7 @@ let models = {
                 if (error) throw error;
                 db.query(`SELECT * FROM products WHERE ID = ${db.escape(object.ID)}`, function (error, r) {
                     if (error) throw error;
-                    if(ur[0].basket != null){
-                        console.log(ur[0].basket[object.ID])
-                        console.log(r[0].limit_amount_per_user)
-                        console.log(ur[0].basket[object.ID] > r[0].limit_amount_per_user)
-                        if(ur[0].basket[object.ID] > r[0].limit_amount_per_user) return resolve({"msg": "reached limit"})
-                    }
+                    if(ur[0].basket != null) if(JSON.parse(ur[0].basket)[object.ID] > r[0].limit_amount_per_user) return resolve({"msg": "reached limit"})
                     if (r[0].amount == 0){
                         resolve({"msg": "product finished"})
                         return;
