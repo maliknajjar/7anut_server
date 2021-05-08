ee5c32fe569b86dfcbdc36227c61d5324508a3ec
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-05-08 16:28:22 +0100

    added user name

--- routes/mainRouters.js
+++ routes/mainRouters.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 // controllers
-let adminController = require("../controllers/adminControllers")
 let usersController = require("../controllers/usersControllers")
 
 //main route
